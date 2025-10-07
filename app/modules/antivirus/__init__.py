1decb3dc717bf213b4dac5f8642f4700d637bc0c
Keyzen2 <keyzen@protonmail.com>
2025-10-07 14:17:15 +0200

    Update __init__.py

--- app/modules/antivirus/__init__.py
+++ app/modules/antivirus/__init__.py
@@ -2,3 +2,8 @@
 Módulo Antivirus de SpamGuard Security Suite
 """
 __version__ = "1.0.0-beta"
+
+from .scanner import FileScanner
+from .signatures import SignatureManager
+
+__all__ = ['FileScanner', 'SignatureManager']
